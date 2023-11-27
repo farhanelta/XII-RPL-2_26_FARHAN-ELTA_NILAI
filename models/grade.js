2f86c1b82c2e5d66dcb4179fde9151535cdfa7b0
farhanelta <farhanelta4@gmail.com>
2023-11-27 17:57:54 +0700

    model update

--- models/grade.js
+++ models/grade.js
@@ -1,23 +1,33 @@
 const mongoose = require('mongoose');
 
 const GradeSchema = new mongoose.Schema({
-    nama: {
+    nis: {
         type: String,
-        required: [true, 'Silahkan isikan nama siswa'],
+        required: [true, 'Silahkan isikan nis siswa'],
         unique: true
     },
     mapel: {
         type: String,
+        required: [true, 'Silahkan isikan mapel siswa'],
+        unique: true
     },
-    nilai_uh: {
+    nilai_pengetahuan: {
         type: String,
     },
-    nilai_praktek: {
+    nilai_keterampilan: {
         type: String,
     },
-    nilai_teori: {
+    nilai_akhir: {
         type: String,
     },
+    nilai_predikat: {
+        type: String,
+    },
+    guru_pengampu: {
+        type: String,
+        required: [true, 'Silahkan isikan nama guru pengampu'],
+        unique: true
+    }
 })
 
 module.exports = mongoose.model('Grade', GradeSchema)
